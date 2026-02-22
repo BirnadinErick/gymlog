22f00d250a3438f4ea8d8a7d8c30ef72616b55ac
BirnadinErick <45619033+BirnadinErick@users.noreply.github.com>
2026-02-22 12:48:26 +0100

    feat: extract workouthead into a component

--- app/record-workout/page.tsx
+++ app/record-workout/page.tsx
@@ -1,15 +1,23 @@
 import WorkoutScreenLayout from "@/components/layouts/workout-screen-layout";
 import RecordWorkoutTopbar from "@/components/record-workout/topbar";
+import WorkoutCardHead from "@/components/record-workout/workout-card-head";
+import dummyWorkouts from "@/lib/dummydata/workouts.json";
 
 export default async function RecordWorkoutPage(props: {
   searchParams: Promise<{ template?: string }>;
 }) {
   const searchParams = await props.searchParams;
-  const templateName = searchParams.template || "Default Workout";
+  const _templateName = searchParams.template || "Empty Session";
 
   return (
     <WorkoutScreenLayout navbarItem={<RecordWorkoutTopbar />}>
-      <h1>{templateName}</h1>
+      <div>
+        <WorkoutCardHead
+          uuid={dummyWorkouts.workouts[0].uuid}
+          heading={dummyWorkouts.workouts[0].heading}
+          imageURL={dummyWorkouts.workouts[0].imageUrl}
+        />
+      </div>
     </WorkoutScreenLayout>
   );
 }
